0c82204c9ea45a94ba54e40a0eab1cb3c6104bcc
Jay Tulshian <tulshianjay@gmail.com>
2021-09-15 22:20:34 +0530

    restructured

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Nav from './components/nav/nav'
+import reactDom from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          class="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+      <Nav />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      </React.Fragment>
+    );
+  }
 }
+
+
 
 export default App;
---
+++ src/components/nav/nav.js
@@ -0,0 +1,24 @@
+export default function Nav() {
+    return (
+      <nav className="navbar navbar-expand-xl navbar-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#"><i className="fas fa-chart-area"></i> botanalytics</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-link m-auto" id="active-link" href="dashboard.html">Dashboard</a>
+              <a className="nav-link m-auto" href="#">Conversations</a>
+              <a className="nav-link m-auto" href="#">Users</a>
+              <a className="nav-link m-auto">Engagement</a>
+              <a className="nav-link m-auto">Integrations</a>
+              <a className="nav-link m-lg-auto">Integrations</a>
+  
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
